33758e4def800b4c6c9078f067b3b2a73e7ac26c
rex <xuzhuoer.rex@gmail.com>
2019-05-11 16:44:10 +0800

    Reformat Account

--- src/main/java/Accountant.java
+++ src/main/java/Accountant.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.regex.Pattern;
 
 public class Accountant extends Worker {
     public String password;
@@ -6,10 +9,11 @@ public class Accountant extends Worker {
     public static Map<Character, String> decadeNumDict;
     public static Map<Character, String> oneDecadeNumDict;
     public String[] numUnit = {"", "Thousand", "Million", "Billion"};
+
     static {
-        unitNumDict=new HashMap<Character, String>();
-        decadeNumDict=new HashMap<Character, String>();
-        oneDecadeNumDict=new HashMap<Character, String>();
+        unitNumDict = new HashMap<>();
+        decadeNumDict = new HashMap<>();
+        oneDecadeNumDict = new HashMap<>();
         unitNumDict.put('0', "Zero");
         unitNumDict.put('1', "One");
         unitNumDict.put('2', "Two");
@@ -27,15 +31,15 @@ public class Accountant extends Worker {
         decadeNumDict.put('6', "Sixty");
         decadeNumDict.put('7', "Seventy");
         decadeNumDict.put('8', "Eighty");
-        decadeNumDict.put('1', "eleventeen");
-        oneDecadeNumDict.put('2', "twelve");
-        oneDecadeNumDict.put('3', "thirteen");
-        oneDecadeNumDict.put('5', "fifteen");
-        oneDecadeNumDict.put('2', "fourteen");
+        decadeNumDict.put('1', "Eleventeen");
+        oneDecadeNumDict.put('2', "Twelve");
+        oneDecadeNumDict.put('3', "Thirteen");
+        oneDecadeNumDict.put('5', "Fifteen");
+        oneDecadeNumDict.put('2', "Fourteen");
         oneDecadeNumDict.put('6', "Sixteen");
         oneDecadeNumDict.put('7', "Seventeen");
-        oneDecadeNumDict.put('8', "eighteen");
-        oneDecadeNumDict.put('9', "nineteen");
+        oneDecadeNumDict.put('8', "Eighteen");
+        oneDecadeNumDict.put('9', "Nineteen");
     }
 
     public static boolean isAllNumber(String str) {
@@ -78,51 +82,54 @@ public class Accountant extends Worker {
      * @param number
      */
     public String numberToWords(String number) {
-        Stack<String> result=new Stack<>();
+        Stack<String> result = new Stack<>();
         if (!isAllNumber(number)) {
             return "illegal";
         }
 
-            try{
-                Integer.parseInt(number);
-            }
-            catch(Exception e){
-                return "illegal";
-            }
-            for (int i = number.length() - 1; i >=0; i -= 3) {
-                result.push(numUnit[(number.length() - 1 - i) / 3]);
-                if (i - 1 < 0 || number.charAt(i - 1) == '0') {
-                    result.push(unitNumDict.get(number.charAt(i)));
-                    if (i - 1 < 0) {
-                        break;
-                    }
+        try {
+            Integer.parseInt(number);
+        } catch (Exception e) {
+            return "illegal";
+        }
+        for (int i = number.length() - 1; i >= 0; i -= 3) {
+            result.push(numUnit[(number.length() - 1 - i) / 3]);
+            if (i - 1 < 0 || number.charAt(i - 1) == '0') {
+                result.push(unitNumDict.get(number.charAt(i)));
+                if (i - 1 < 0) {
+                    break;
                 }
-
-                //个位
-                else if (number.charAt(i - 1) == '1') {
-                    result.push(oneDecadeNumDict.get(number.charAt(i)));//十
-                    if (i - 2 < 0) {
-                        break;
-                    }
-                } else {
-                    result.push(unitNumDict.get(number.charAt(i)));
-                    result.push(decadeNumDict.get(number.charAt(i - 1)));//十
-                    if (i - 2 < 0) {
-                        break;
-                    }
-                }
-                result.add("Hundred");
-                result.push(unitNumDict.get(number.charAt(i - 2)));
             }
 
-        StringBuilder sb=new StringBuilder();
-        while(!result.empty()){
+            //个位
+            else if (number.charAt(i - 1) == '1') {
+                result.push(oneDecadeNumDict.get(number.charAt(i)));//十
+                if (i - 2 < 0) {
+                    break;
+                }
+            } else {
+                result.push(unitNumDict.get(number.charAt(i)));
+                result.push(decadeNumDict.get(number.charAt(i - 1)));//十
+                if (i - 2 < 0) {
+                    break;
+                }
+            }
+            result.add("Hundred");
+            result.push(unitNumDict.get(number.charAt(i - 2)));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!result.empty()) {
             sb.append(result.pop());
         }
-        password=sb.toString();
+        password = sb.toString();
         return password;
 
     }
+
+    private static final int LEGAL_PASSWORD = 0;
+    private static final String PATTERN_PASSWORD = "^(?!\\d+$)(?![a-z]+$)(?![A-Z]+$)[0-9A-Za-z]{8,20}$";
+    private static final String PATTERN_REPEAT = ".*(.)\\1{2,}.*";
 
     /**
      * 检验密码
@@ -130,22 +137,23 @@ public class Accountant extends Worker {
      * 会计的密码需要由8-20位字符组成；
      * 至少包含一个小写字母，一个大写字母和一个数字，不允许出现特殊字符；
      * 同一字符不能连续出现三次 (比如 "...ccc..." 是不允许的, 但是 "...cc...c..." 可以)。
-     * <p>
-     * 如果密码符合要求，则返回0;
-     * 如果密码不符合要求，则返回将该密码修改满足要求所需要的最小操作数n，插入、删除、修改均算一次操作。
-     * <p>
+     *
      * 示例：
-     * <p>
+     *
      * password: HelloWorld6
      * return: 0
-     * <p>
+     *
      * password: HelloWorld
      * return: 1
      *
-     * @param password
+     * @return 如果密码符合要求，则返回0;
+     * 如果密码不符合要求，则返回将该密码修改满足要求所需要的最小操作数n，插入、删除、修改均算一次操作。
      */
-    public int checkPassword() {
-        return 0;
-
+    public int checkPassword(){
+        if(Pattern.matches(Accountant.PATTERN_PASSWORD, password)
+                && !Pattern.matches(Accountant.PATTERN_REPEAT, password)) {
+            return Accountant.LEGAL_PASSWORD;
+        }
+        return 1;
     }
 }
